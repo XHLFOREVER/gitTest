a51ad31062fb55e8ba4bab9c463489a1b296ea0c
XiaoHL <XiaoHL@atguigu.com>
2021-04-15 20:37:15 +0800

    push commit

--- src/main/java/com/atguigu/Test.java
+++ src/main/java/com/atguigu/Test.java
@@ -3,5 +3,7 @@ package com.atguigu;
 public class Test {
     public static void main(String[] args) {
         System.out.println("温二狗");
+        System.out.println("pull");
+        System.out.println("push");
     }
 }
